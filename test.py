8fa45f372a73dde31f40fdb53250eb3f07f4d80c
liuchangyu <liuchangyu1111@gmail.com>
2020-05-18 08:48:13 +0800

    add test file and test method

--- test.py
+++ test.py
@@ -16,42 +16,65 @@ import os
 import random
 
 import torch.backends.cudnn as cudnn
-import torch.nn as nn
-import torch.utils.data
 import torch.utils.data.distributed
-import torchvision
-import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
-from tqdm import tqdm
+from PIL import Image
 
 from srgan_pytorch import Discriminator
-from srgan_pytorch import FeatureExtractor
 from srgan_pytorch import Generator
-from srgan_pytorch import weights_init
 
 parser = argparse.ArgumentParser(description="PyTorch Super Resolution GAN.")
-parser.add_argument("--dataroot", type=str, default="./data",
-                    help="Path to datasets. (default:`./data`)")
-parser.add_argument("-j", "--workers", default=8, type=int, metavar="N",
-                    help="Number of data loading workers. (default:8)")
-parser.add_argument("--epochs", default=2000, type=int, metavar="N",
-                    help="Number of total epochs to run. (default:2000)")
+parser.add_argument("--file", type=str, default="./data/test.png",
+                    help="Path to image. (default:`./data/test.png`)")
+parser.add_argument("-j", "--workers", default=4, type=int, metavar="N",
+                    help="Number of data loading workers. (default:4)")
 parser.add_argument("--image-size", type=int, default=96,
                     help="Size of the data crop (squared assumed). (default:96)")
-parser.add_argument("-b", "--batch-size", default=8, type=int,
-                    metavar="N",
-                    help="mini-batch size (default: 8), this is the total "
-                         "batch size of all GPUs on the current node when "
-                         "using Data Parallel or Distributed Data Parallel.")
-parser.add_argument("--lr", type=float, default=0.0001,
-                    help="Learning rate. (default:0.0001)")
 parser.add_argument("--up-sampling", type=int, default=4,
                     help="Low to high resolution scaling factor. (default:4).")
 parser.add_argument("--cuda", action="store_true", help="Enables cuda")
-parser.add_argument("--netG", default="", help="Path to netG (to continue training).")
-parser.add_argument("--netD", default="", help="Path to netD (to continue training).")
+parser.add_argument("--netG", default="./weights/netG.pth", help="Path to netG (default:`./weights/netG.pth`).")
+parser.add_argument("--netD", default="./weights/netD.pth", help="Path to netD (default:`./weights/netD.pth`).")
 parser.add_argument("--outf", default="./results",
                     help="folder to output images. (default:`./outputs`).")
 parser.add_argument("--manualSeed", type=int,
                     help="Seed for initializing training. (default:none)")
+
+args = parser.parse_args()
+print(args)
+
+try:
+    os.makedirs(args.outf)
+except OSError:
+    pass
+
+if args.manualSeed is None:
+    args.manualSeed = random.randint(1, 10000)
+print("Random Seed: ", args.manualSeed)
+random.seed(args.manualSeed)
+torch.manual_seed(args.manualSeed)
+
+cudnn.benchmark = True
+
+if torch.cuda.is_available() and not args.cuda:
+    print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+
+device = torch.device("cuda:0" if args.cuda else "cpu")
+
+resize = transforms.Compose([transforms.Resize(args.image_size),
+                             transforms.ToTensor(),
+                             transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                  std=[0.229, 0.224, 0.225])
+                             ])
+
+image = Image.open(args.file)
+image = resize(image)
+
+generator = Generator(8, args.up_sampling).to(device)
+discriminator = Discriminator().to(device)
+
+high_resolution_fake_image = generator(image)
+vutils.save_image(high_resolution_fake_image.detach(),
+                  f"{args.outf}/fake.png",
+                  normalize=True)
