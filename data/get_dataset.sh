f7a110fe7528ce1514ba02965f8c95fb143bf14c
liuchangyu <liuchangyu1111@gmail.com>
2020-05-18 11:45:09 +0800

    load download

--- data/get_dataset.sh
+++ data/get_dataset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# wget https://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_bicubic_X4.zip
+wget https://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_bicubic_X4.zip
 unzip DIV2K_train_LR_bicubic_X4.zip
 mv DIV2K_train_LR_bicubic/X4 ./
 rm -rf DIV2K_train_LR_bicubic
