cb04075c03b2a9f9b49c1e6d6edb8de1f6647249
LiuChangyu <liuchangyu1111@gmail.com>
2020-09-02 08:49:21 +0800

    Method of adding evaluation index

--- srgan_pytorch/utils.py
+++ srgan_pytorch/utils.py
@@ -12,9 +12,12 @@
 # limitations under the License.
 # ==============================================================================
 import math
+from math import exp
 
-import cv2
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
 from torchvision.models import vgg19
 
 
@@ -28,21 +31,20 @@ class FeatureExtractor(nn.Module):
         return self.features(x)
 
 
-def evaluate_performance(real_image, fake_image):
-    prediction = cv2.imread(fake_image)
-    target = cv2.imread(real_image)
+def evaluate_performance(img1, img2, window_size=11, size_average=True):
+    mse = ((img1 - img2) ** 2).data.mean()
+    psnr = 10 * math.log10(255.0 ** 2 / mse)
 
-    error_value = []
-    for i in range(len(target)):
-        error_value.append((target[i] - prediction[i]) ** 2)
+    (_, channel, _, _) = img1.size()
+    window = create_window(window_size, channel)
 
-    mse = sum(error_value) / len(error_value)
+    if img1.is_cuda:
+        window = window.cuda(img1.get_device())
+    window = window.type_as(img1)
 
-    if mse < 1.0e-10:
-        return 100
-    psnr = 20 * math.log10(1 / math.sqrt(mse))
+    ssim = _ssim(img1, img2, window, window_size, channel, size_average)
 
-    return mse, psnr
+    return psnr, ssim
 
 
 # custom weights initialization called on netG and netD
@@ -53,3 +55,64 @@ def weights_init(m):
     elif classname.find("BatchNorm") != -1:
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
+
+
+def gaussian(window_size, sigma):
+    gauss = torch.Tensor([exp(-(x - window_size // 2) ** 2 / float(2 * sigma ** 2)) for x in range(window_size)])
+    return gauss / gauss.sum()
+
+
+def create_window(window_size, channel):
+    _1D_window = gaussian(window_size, 1.5).unsqueeze(1)
+    _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
+    window = Variable(_2D_window.expand(channel, 1, window_size, window_size).contiguous())
+    return window
+
+
+def _ssim(img1, img2, window, window_size, channel, size_average=True):
+    mu1 = F.conv2d(img1, window, padding=window_size // 2, groups=channel)
+    mu2 = F.conv2d(img2, window, padding=window_size // 2, groups=channel)
+
+    mu1_sq = mu1.pow(2)
+    mu2_sq = mu2.pow(2)
+    mu1_mu2 = mu1 * mu2
+
+    sigma1_sq = F.conv2d(img1 * img1, window, padding=window_size // 2, groups=channel) - mu1_sq
+    sigma2_sq = F.conv2d(img2 * img2, window, padding=window_size // 2, groups=channel) - mu2_sq
+    sigma12 = F.conv2d(img1 * img2, window, padding=window_size // 2, groups=channel) - mu1_mu2
+
+    C1 = 0.01 ** 2
+    C2 = 0.03 ** 2
+
+    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) * (sigma1_sq + sigma2_sq + C2))
+
+    if size_average:
+        return ssim_map.mean()
+    else:
+        return ssim_map.mean(1).mean(1).mean(1)
+
+
+class SSIM(torch.nn.Module):
+    def __init__(self, window_size=11, size_average=True):
+        super(SSIM, self).__init__()
+        self.window_size = window_size
+        self.size_average = size_average
+        self.channel = 1
+        self.window = create_window(window_size, self.channel)
+
+    def forward(self, img1, img2):
+        (_, channel, _, _) = img1.size()
+
+        if channel == self.channel and self.window.data.type() == img1.data.type():
+            window = self.window
+        else:
+            window = create_window(self.window_size, channel)
+
+            if img1.is_cuda:
+                window = window.cuda(img1.get_device())
+            window = window.type_as(img1)
+
+            self.window = window
+            self.channel = channel
+
+        return _ssim(img1, img2, window, self.window_size, channel, self.size_average)
