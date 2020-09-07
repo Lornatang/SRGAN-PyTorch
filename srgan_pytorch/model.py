580479f84e57f5bf66b03c364ee45cdadd38c12c
Liu Changyu <liuchangyu1111@gmail.com>
2020-09-07 09:47:44 +0800

    Update model.py

--- srgan_pytorch/model.py
+++ srgan_pytorch/model.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Lorna Authors. All Rights Reserved.
+# Copyright 2020 Dakewe Biotech Corporation. All Rights Reserved.
 # Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
 #   You may obtain a copy of the License at
@@ -15,7 +15,89 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .acitivity import swish
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels=64, out_channels=64, kernel_size=3,
+                 stride=1):
+        super(ResidualBlock, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size,
+                               stride=stride, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size,
+                               stride=stride, padding=1)
+        self.bn = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        shortcut = x
+
+        x = self.conv1(x)
+        x = self.bn(x)
+        x = F.relu(x, inplace=True)
+
+        x = self.conv2(x)
+        x = self.bn(x)
+
+        out = x + shortcut
+
+        return out
+
+
+class UpsampleBlock(nn.Module):
+    # Implements resize-convolution
+    def __init__(self, in_channels, out_channels):
+        super(UpsampleBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3,
+                              stride=1, padding=1)
+        self.shuffler = nn.PixelShuffle(2)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.shuffler(x)
+
+        out = F.relu(x, inplace=True)
+        return out
+
+
+class Generator(nn.Module):
+    def __init__(self, n_residual_blocks, upsample_factor):
+        super(Generator, self).__init__()
+        self.n_residual_blocks = n_residual_blocks
+        self.upsample_factor = upsample_factor
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=9, stride=1, padding=4)
+
+        for i in range(self.n_residual_blocks):
+            self.add_module("residual_block_" + str(i + 1), ResidualBlock())
+
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.bn = nn.BatchNorm2d(64)
+
+        for i in range(self.upsample_factor // 2):
+            self.add_module("upsample_block_" + str(i + 1),
+                            UpsampleBlock(64, 256))
+
+        self.conv3 = nn.Conv2d(64, 3, kernel_size=9, stride=1, padding=4)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x, inplace=True)
+
+        y = x.clone()
+        for i in range(self.n_residual_blocks):
+            y = self.__getattr__("residual_block_" + str(i + 1))(y)
+
+        shortcut = x
+
+        x = self.conv2(y)
+        x = self.bn(x)
+        x = x + shortcut
+
+        for i in range(self.upsample_factor // 2):
+            x = self.__getattr__("upsample_block_" + str(i + 1))(x)
+
+        out = self.conv3(x)
+
+        return out
 
 
 class Discriminator(nn.Module):
@@ -43,117 +125,39 @@ class Discriminator(nn.Module):
 
     def forward(self, x):
         x = self.conv1(x)
-        x = swish(x)
+        x = F.relu(x, inplace=True)
 
         x = self.conv2(x)
         x = self.bn2(x)
-        x = swish(x)
+        x = F.relu(x, inplace=True)
 
         x = self.conv3(x)
         x = self.bn3(x)
-        x = swish(x)
+        x = F.relu(x, inplace=True)
 
         x = self.conv4(x)
         x = self.bn4(x)
-        x = swish(x)
+        x = F.relu(x, inplace=True)
 
         x = self.conv5(x)
         x = self.bn5(x)
-        x = swish(x)
+        x = F.relu(x, inplace=True)
 
         x = self.conv6(x)
         x = self.bn6(x)
-        x = swish(x)
+        x = F.relu(x, inplace=True)
 
         x = self.conv7(x)
         x = self.bn7(x)
-        x = swish(x)
+        x = F.relu(x, inplace=True)
 
         x = self.conv8(x)
         x = self.bn8(x)
-        x = swish(x)
+        x = F.relu(x, inplace=True)
 
         x = self.conv9(x)
 
         x = F.avg_pool2d(x, x.size()[2:])
         x = torch.sigmoid(x)
         out = torch.flatten(x, 1)
-        return out
-
-
-class Generator(nn.Module):
-    def __init__(self, scale_factor):
-        super(Generator, self).__init__()
-        self.scale_factor = scale_factor
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=9, stride=1, padding=4)
-
-        for i in range(8):
-            self.add_module("residual_block_" + str(i + 1), ResidualBlock())
-
-        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.bn = nn.BatchNorm2d(64)
-
-        for i in range(self.scale_factor // 2):
-            self.add_module("scale_block_" + str(i + 1), UpsampleBlock(64, 256))
-
-        self.conv3 = nn.Conv2d(64, 3, kernel_size=9, stride=1, padding=4)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = swish(x)
-
-        y = x.clone()
-        for i in range(8):
-            y = self.__getattr__("residual_block_" + str(i + 1))(y)
-
-        shortcut = x
-
-        x = self.conv2(y)
-        x = self.bn(x)
-        x = x + shortcut
-
-        for i in range(self.scale_factor // 2):
-            x = self.__getattr__("scale_block_" + str(i + 1))(x)
-
-        out = self.conv3(x)
-
-        return out
-
-
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels=64, out_channels=64, kernel_size=3, stride=1):
-        super(ResidualBlock, self).__init__()
-
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=1)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size, stride=stride, padding=1)
-        self.bn = nn.BatchNorm2d(out_channels)
-
-    def forward(self, x):
-        shortcut = x
-
-        x = self.conv1(x)
-        x = self.bn(x)
-        x = swish(x)
-
-        x = self.conv2(x)
-        x = self.bn(x)
-
-        out = x + shortcut
-
-        return out
-
-
-class UpsampleBlock(nn.Module):
-    # Implements resize-convolution
-    def __init__(self, in_channels, out_channels):
-        super(UpsampleBlock, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
-        self.shuffler = nn.PixelShuffle(2)
-
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.shuffler(x)
-
-        out = swish(x)
         return out
