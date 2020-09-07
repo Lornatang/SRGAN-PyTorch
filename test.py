bf56206cf3f28d72173608b6ca06c9a5587f86f8
Liu Changyu <liuchangyu1111@gmail.com>
2020-09-07 09:50:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Lorna Authors. All Rights Reserved.
+# Copyright 2020 Dakewe Biotech Corporation. All Rights Reserved.
 # Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
 #   You may obtain a copy of the License at
@@ -20,20 +20,25 @@ import torch.utils.data.distributed
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
-from tqdm import tqdm
 
 from srgan_pytorch import Generator
+from srgan_pytorch import cal_psnr
+from srgan_pytorch import cal_ssim
 
 parser = argparse.ArgumentParser(description="PyTorch Super Resolution GAN.")
 parser.add_argument("--dataroot", type=str, default="./data",
                     help="Path to dataset. (default:`./data`)")
-parser.add_argument("--image-size", type=int, default=88,
-                    help="Size of the data crop (squared assumed). (default:88)")
-parser.add_argument("--upscale-factor", type=int, default=4,
-                    help="Super resolution upscale factor. (default:4).")
+parser.add_argument("-j", "--workers", default=0, type=int, metavar="N",
+                    help="Number of data loading workers. (default:0)")
+parser.add_argument("--image-size", type=int, default=96,
+                    help="Size of the data crop (squared assumed). (default:96)")
+parser.add_argument("--scale-factor", type=int, default=4,
+                    help="Low to high resolution scaling factor. (default:4).")
 parser.add_argument("--cuda", action="store_true", help="Enables cuda")
+parser.add_argument("--weights", default="./weights/netG_X4.pth",
+                    help="Path to netG (default:`./weights/netG_X4.pth`).")
 parser.add_argument("--outf", default="./results",
-                    help="folder to output images. (default:`./results`).")
+                    help="folder to output images. (default:`./outputs`).")
 parser.add_argument("--manualSeed", type=int,
                     help="Seed for initializing training. (default:none)")
 
@@ -54,42 +59,64 @@ torch.manual_seed(args.manualSeed)
 cudnn.benchmark = True
 
 if torch.cuda.is_available() and not args.cuda:
-    print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+    print("WARNING: You have a CUDA device, "
+          "so you should probably run with --cuda")
 
+# Load dataset
 dataset = datasets.ImageFolder(root=args.dataroot,
                                transform=transforms.Compose([
-                                   transforms.RandomResizedCrop(args.image_size * args.upscale_factor),
+                                   transforms.RandomResizedCrop(
+                                       args.image_size * args.scale_factor),
                                    transforms.ToTensor()]))
 
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, pin_memory=True)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=1,
+                                         shuffle=False, pin_memory=True,
+                                         num_workers=int(args.workers))
 
+# Setting device
 device = torch.device("cuda:0" if args.cuda else "cpu")
 
-# create model
-model = Generator(scale_factor=args.upscale_factor).to(device)
+# Load model
+model = Generator(8, args.scale_factor).to(device)
+model.load_state_dict(torch.load(args.weights))
 
-# Load state dicts
-model.load_state_dict(torch.load("weights/netG.pth", map_location=device))
+resize = transforms.Compose([transforms.ToPILImage(),
+                             transforms.Resize(args.image_size),
+                             transforms.ToTensor(),
+                             transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                  std=[0.229, 0.224, 0.225]),
+                             ])
 
-# Set model mode
-model.eval()
+total_psnr_value = 0.0
+total_ssim_value = 0.0
 
-resize = transforms.Compose([transforms.Resize(args.image_size),
-                             transforms.ToTensor()])
+for i, data in enumerate(dataloader):
+    hr_real_image = data[0].to(device)
+    batch_size = hr_real_image.size(0)
 
-progress_bar = tqdm(enumerate(dataloader), total=len(dataloader))
-for i, data in progress_bar:
-    high_resolution_real_image = data[0].to(device)
-    batch_size = high_resolution_real_image.size(0)
+    lr_real_image = torch.randn(batch_size, 3, args.image_size,
+                                args.image_size, device=device)
 
     # Down sample images to low resolution
-    low_resolution_image = resize(high_resolution_real_image)
+    for batch_index in range(batch_size):
+        lr_real_image[batch_index] = resize(hr_real_image[batch_index].cpu())
 
     # Generate real and fake inputs
-    high_resolution_fake_image = model(low_resolution_image)
+    hr_fake_image = model(lr_real_image)
 
-    vutils.save_image(high_resolution_real_image, f"{args.outf}/high_res_real_{i}.png", normalize=False)
-    vutils.save_image(high_resolution_fake_image.detach(), f"{args.outf}/high_res_fake_{i}.png", normalize=True)
-    vutils.save_image(low_resolution_image, f"{args.outf}/low_res_real_{i}.png", normalize=True)
+    vutils.save_image(hr_real_image,
+                      f"{args.outf}/hr_real_{i}.png",
+                      normalize=True)
+    vutils.save_image(hr_fake_image,
+                      f"{args.outf}/hr_fake_{i}.png", normalize=True)
+    vutils.save_image(lr_real_image,
+                      f"{args.outf}/lr_real_{i}.png",
+                      normalize=True)
 
-    progress_bar.set_description(f"[Number of currently processed pictures: {i} images]")
+    total_psnr_value += cal_psnr(f"{args.outf}/hr_real_{i}.png",
+                                 f"{args.outf}/hr_fake_{i}.png")
+    total_ssim_value += cal_ssim(f"{args.outf}/hr_real_{i}.png",
+                                 f"{args.outf}/hr_fake_{i}.png")
+
+print(f"Avg PSNR: {total_psnr_value / len(dataloader):.2f} "
+      f"Avg SSIM: {total_ssim_value / len(dataloader):.4f}")
