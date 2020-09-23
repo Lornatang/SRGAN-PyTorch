efe0cba247cce94154bd77a726fb257359bc7b6a
Liu Changyu <liuchangyu1111@gmail.com>
2020-09-23 19:03:50 +0800

    Update download_weights.sh

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget https://github.com/Lornatang/SRGAN-PyTorch/releases/download/1.1/pretrained_X4.pth
+wget https://github.com/Lornatang/SRGAN-PyTorch/releases/download/1.1/SRResNet_X4.pth
 wget https://github.com/Lornatang/SRGAN-PyTorch/releases/download/1.1/SRGAN_X4.pth
