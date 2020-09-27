87449087326cab105bb94af986c5e4711fbb109a
Changyu <liuchangyu1111@gmail.com>
2020-09-27 09:05:26 +0800

    update

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "Start downloading pre training weights."
 wget https://github.com/Lornatang/SRGAN-PyTorch/releases/download/1.1/SRResNet_X4.pth
 wget https://github.com/Lornatang/SRGAN-PyTorch/releases/download/1.1/SRGAN_X4.pth
+echo "Download weights done!"
