fada7bb811441f1d0c1d914f5d5cb6e45b96c13d
Changyu <liuchangyu1111@gmail.com>
2020-10-13 09:44:06 +0800

    Reduce model capacity

--- srgan_pytorch/model.py
+++ srgan_pytorch/model.py
@@ -32,35 +32,35 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, padding=1),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
 
             nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(64),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
 
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
 
             nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
 
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
 
             nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
 
             nn.Conv2d(256, 512, kernel_size=3, padding=1),
             nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
 
             nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2)
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
         )
 
         self.avgpool = nn.AdaptiveAvgPool2d(1)
