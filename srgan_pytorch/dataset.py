103b9170b22363d4b393648493ac19d52b316710
Oktay Ozturk <oktay.ozturk010@gmail.com>
2021-04-05 11:52:19 +0300

    Add tiff file support

--- srgan_pytorch/dataset.py
+++ srgan_pytorch/dataset.py
@@ -35,7 +35,7 @@ def check_image_file(filename):
     Returns:
         Return True if bool(x) is True for any x in the iterable.
     """
-    return any(filename.endswith(extension) for extension in [".jpg", ".jpeg", ".png", ".bmp", ".JPG", ".JPEG", ".PNG", ".BMP"])
+    return any(filename.endswith(extension) for extension in [".jpg", ".jpeg", ".png", ".bmp", ".tif", ".tiff", ".JPG", ".JPEG", ".PNG", ".BMP"])
 
 
 class BaseTrainDataset(torch.utils.data.dataset.Dataset):
