c73daa978682ecfee3ddbcf996fdc15abe73e130
Liu <liuchangyu1111@gmail.com>
2021-04-07 10:54:40 +0800

    Add a new optimizer strategy Adam belief

--- srgan_pytorch/optimizer/lr_scheduler.py
+++ srgan_pytorch/optimizer/lr_scheduler.py
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Copy from https://github.com/dkumazaw/onecyclelr/blob/master/onecyclelr.py"""
+"""Source code copy from `https://github.com/dkumazaw/onecyclelr/blob/master/onecyclelr.py`"""
 from torch.optim import Optimizer
 
 
@@ -23,6 +23,7 @@ class OneCycleLR:
     So, the learning rate changes like min_lr -> max_lr -> min_lr -> final_lr,
     where final_lr = min_lr * reduce_factor.
     Note: Currently only supports one parameter group.
+
     Args:
         optimizer:             (Optimizer) against which we apply this scheduler
         num_steps:             (int) of total number of steps/iterations
@@ -31,6 +32,7 @@ class OneCycleLR:
         annihilation_frac:     (float), fracion of steps to annihilate the learning rate
         reduce_factor:         (float), denotes the factor by which we annihilate the learning rate at the end
         last_step:             (int), denotes the last step. Set to -1 to start training from the beginning
+
     Example:
         >>> optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
         >>> scheduler = OneCycleLR(optimizer, num_steps=num_steps, lr_range=(0.1, 1.))
@@ -53,18 +55,16 @@ class OneCycleLR:
                  last_step: int = -1):
         # Sanity check
         if not isinstance(optimizer, Optimizer):
-            raise TypeError('{} is not an Optimizer'.format(type(optimizer).__name__))
+            raise TypeError("{} is not an Optimizer".format(type(optimizer).__name__))
         self.optimizer = optimizer
 
         self.num_steps = num_steps
 
         self.min_lr, self.max_lr = lr_range[0], lr_range[1]
-        assert self.min_lr < self.max_lr, \
-            "Argument lr_range must be (min_lr, max_lr), where min_lr < max_lr"
+        assert self.min_lr < self.max_lr, "Argument lr_range must be (min_lr, max_lr), where min_lr < max_lr"
 
         self.min_momentum, self.max_momentum = momentum_range[0], momentum_range[1]
-        assert self.min_momentum < self.max_momentum, \
-            "Argument momentum_range must be (min_momentum, max_momentum), where min_momentum < max_momentum"
+        assert self.min_momentum < self.max_momentum, "Argument momentum_range must be (min_momentum, max_momentum), where min_momentum < max_momentum"
 
         self.num_cycle_steps = int(num_steps * (1. - annihilation_frac))  # Total number of steps in the cycle
         self.final_lr = self.min_lr * reduce_factor
@@ -79,7 +79,7 @@ class OneCycleLR:
         It contains an entry for every variable in self.__dict__ which
         is not the optimizer. (Borrowed from _LRScheduler class in torch.optim.lr_scheduler.py)
         """
-        return {key: value for key, value in self.__dict__.items() if key != 'optimizer'}
+        return {key: value for key, value in self.__dict__.items() if key != "optimizer"}
 
     def load_state_dict(self, state_dict):
         """Loads the schedulers state. (Borrowed from _LRScheduler class in torch.optim.lr_scheduler.py)
@@ -90,10 +90,10 @@ class OneCycleLR:
         self.__dict__.update(state_dict)
 
     def get_lr(self):
-        return self.optimizer.param_groups[0]['lr']
+        return self.optimizer.param_groups[0]["lr"]
 
     def get_momentum(self):
-        return self.optimizer.param_groups[0]['momentum']
+        return self.optimizer.param_groups[0]["momentum"]
 
     def step(self):
         """Conducts one step of learning rate and momentum update
@@ -120,6 +120,6 @@ class OneCycleLR:
             # Exceeded given num_steps: do nothing
             return
 
-        self.optimizer.param_groups[0]['lr'] = lr
+        self.optimizer.param_groups[0]["lr"] = lr
         if momentum:
-            self.optimizer.param_groups[0]['momentum'] = momentum
+            self.optimizer.param_groups[0]["momentum"] = momentum
