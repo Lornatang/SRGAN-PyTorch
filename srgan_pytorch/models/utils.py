0f05f851590c8461c3099530984508d0a137b975
Liu <liuchangyu1111@gmail.com>
2021-04-09 14:58:33 +0800

    Fix state dict error

--- srgan_pytorch/models/utils.py
+++ srgan_pytorch/models/utils.py
@@ -43,14 +43,14 @@ class ResidualBlock(nn.Module):
         super(ResidualBlock, self).__init__()
         self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(channels)
-        self.prelu1 = nn.PReLU()
+        self.prelu = nn.PReLU()
         self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(channels)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         out = self.conv1(x)
         out = self.bn1(out)
-        out = self.prelu1(out)
+        out = self.prelu(out)
         out = self.conv2(out)
         out = self.bn2(out)
 
