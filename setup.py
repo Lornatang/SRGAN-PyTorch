ddd8f00046561dd4147036b77cd0aa7505e762d1
Liu <liuchangyu1111@gmail.com>
2021-04-15 20:00:17 +0800

    Upgrade code version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ URL = "https://github.com/Lornatang/SRGAN-PyTorch"
 EMAIL = "liu_changyu@dakewe.com"
 AUTHOR = "Liu Goodfellow"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.2.1"
+VERSION = "0.2.2"
 
 # Libraries that must be installed.
 REQUIRED = [
