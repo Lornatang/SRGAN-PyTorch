6b0a9f1cb9b0e6fe1c9239e05054d4eae0b3194f
Liu <liuchangyu1111@gmail.com>
2021-04-27 14:56:22 +0800

    Add data augment operator

--- srgan_pytorch/utils/__init__.py
+++ srgan_pytorch/utils/__init__.py
@@ -12,6 +12,7 @@
 # limitations under the License.
 # ==============================================================================
 from .common import *
+from .data_augmentation import *
 from .estimate import *
 from .image_quality_assessment import *
 from .transform import *
