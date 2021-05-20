e7894dbef09f68d62b11290f0a0631ff295d2683
Liu <liuchangyu1111@gmail.com>
2021-05-20 17:07:57 +0800

    Rename MSSIM to MS-SSIM

--- srgan_pytorch/utils/image_quality_assessment/__init__.py
+++ srgan_pytorch/utils/image_quality_assessment/__init__.py
@@ -13,7 +13,7 @@
 # ==============================================================================
 from .calculate_gmsd import *
 from .calculate_lpips import *
-from .calculate_mssim import *
+from .calculate_msssim import *
 from .calculate_niqe import *
 from .calculate_psnr import *
 from .calculate_ssim import *
