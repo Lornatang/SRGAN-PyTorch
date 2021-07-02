2fdd4f3c109eee99f2d13f819a1df7c9ee2a2d32
Liu Changyu <liuchangyu1111@gmail.com>
2021-07-02 13:28:07 +0800

    Update visual_feature_layer.py

--- scripts/visual_feature_layer.py
+++ scripts/visual_feature_layer.py
@@ -22,16 +22,11 @@ torch.manual_seed(666)
 
 image = transforms.ToTensor()(Image.open("visual/SR/104_27.png"))
 
-mean = torch.Tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1)
-std = torch.Tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1)
-
 model = models.vgg19(pretrained=True)
 model = torch.nn.Sequential(*list(model.features.children())[:35]).eval()
 
 for name, parameters in model.named_parameters():
     parameters.requires_grad = False
-
-image = (image - mean) / std
 
 features = model(image).squeeze(0)
 
