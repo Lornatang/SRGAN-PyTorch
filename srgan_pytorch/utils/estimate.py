9ed2a21c75ce999821051f4b189b700e10b8145f
Liu Changyu <liuchangyu1111@gmail.com>
2021-07-06 13:11:15 +0800

    Update estimate.py

--- srgan_pytorch/utils/estimate.py
+++ srgan_pytorch/utils/estimate.py
@@ -17,76 +17,97 @@ import torch.utils.data
 from tqdm import tqdm
 
 from .image_quality_assessment import GMSD
+from .image_quality_assessment import LPIPS
+from .image_quality_assessment import MS_SSIM
+from .image_quality_assessment import PSNR
 from .image_quality_assessment import SSIM
 
-__all__ = [
-    "iqa", "test"
-]
+__all__ = ["iqa", "test"]
 
 
-def iqa(source: torch.Tensor, target: torch.Tensor, gpu: int) -> [torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+def iqa(source, target, gpu):
     """Image quality evaluation function.
 
     Args:
-        source (torch.Tensor): Original tensor image data.
-        target (torch.Tensor): Target tensor image data.
+        source (torch.Tensor): Original tensor image.
+        target (torch.Tensor): Target tensor image.
         gpu (int): Graphics card index.
 
     Returns:
-        MSE, RMSE, PSNR, SSIM, GMSD.
+        MSE, RMSE, PSNR(RGB channel), SSIM, MS-SSIM, LPIPS, GMSD.
     """
     mse_loss = nn.MSELoss().cuda(gpu).eval()
-    # Reference sources from https://github.com/dingkeyan93/IQA-optimization/blob/master/IQA_pytorch/SSIM.py
+    psnr_loss = PSNR(y_channel=False,gpu=gpu).cuda(gpu).eval()
+    # Source code reference from `https://github.com/richzhang/PerceptualSimilarity`
+    lpips_loss = LPIPS(gpu).cuda(gpu).eval()
+    # Source code reference from `https://github.com/dingkeyan93/IQA-optimization/blob/master/IQA_pytorch/SSIM.py`
     ssim_loss = SSIM().cuda(gpu).eval()
-    # Reference sources from http://www4.comp.polyu.edu.hk/~cslzhang/IQA/GMSD/GMSD.htm
+    # Source code reference from `https://github.com/dingkeyan93/IQA-optimization/blob/master/IQA_pytorch/MS_SSIM.py`
+    msssim_loss = MS_SSIM().cuda(gpu).eval()
+    # Source code reference from `http://www4.comp.polyu.edu.hk/~cslzhang/IQA/GMSD/GMSD.htm`
     gmsd_loss = GMSD().cuda(gpu).eval()
+
+    # Convert the image value range to [0, 1].
+    source = (source + 1) / 2
+    target = (target + 1) / 2
 
     # Complete estimate.
     with torch.no_grad():
         mse_value = mse_loss(source, target)
         rmse_value = torch.sqrt(mse_value)
-        psnr_value = 10 * torch.log10(1. / mse_value)
+        psnr_value = psnr_loss(source, target)
         ssim_value = ssim_loss(source, target)
+        msssim_value = msssim_loss(source, target)
+        lpips_value = lpips_loss(source, target)
         gmsd_value = gmsd_loss(source, target)
 
-    return mse_value, rmse_value, psnr_value, ssim_value, gmsd_value
+    return mse_value, rmse_value, psnr_value, ssim_value, msssim_value, lpips_value, gmsd_value
 
 
-def test(dataloader: torch.utils.data.DataLoader, model: nn.Module, gpu: int) -> [torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-    mse_loss = nn.MSELoss().cuda(gpu).eval()
-    # Reference sources from https://hub.fastgit.org/dingkeyan93/IQA-optimization/blob/master/IQA_pytorch/SSIM.py
+def test(dataloader, model, gpu):
+    """Image quality evaluation function (dataloader).
+
+    Args:
+        dataloader (torch.utils.data.DataLoader): Test dataset loader.
+        model (nn.Module): Super-resolution model.
+        gpu (int): Graphics card index.
+
+    Returns:
+        PSNR(RGB channel), SSIM.
+    """
+    psnr_loss = PSNR(y_channel=False,gpu=gpu).cuda(gpu).eval()
+    # Source code reference from `https://github.com/dingkeyan93/IQA-optimization/blob/master/IQA_pytorch/SSIM.py`
     ssim_loss = SSIM().cuda(gpu).eval()
-    # Reference sources from http://www4.comp.polyu.edu.hk/~cslzhang/IQA/GMSD/GMSD.htm
-    gmsd_loss = GMSD().cuda(gpu).eval()
 
     # switch eval mode.
     model.eval()
 
-    progress_bar = tqdm(enumerate(dataloader), total=len(dataloader))
+    total = len(dataloader)
+    progress_bar = tqdm(enumerate(dataloader), total=total)
     total_psnr_value = 0.
     total_ssim_value = 0.
-    total_gmsd_value = 0.
-    total = len(dataloader)
 
     with torch.no_grad():
-        for i, (lr, _, hr) in progress_bar:
+        for i, (lr, hr) in progress_bar:
             # Move data to special device.
             if gpu is not None:
                 lr = lr.cuda(gpu, non_blocking=True)
-            if torch.cuda.is_available():
                 hr = hr.cuda(gpu, non_blocking=True)
 
             sr = model(lr)
 
+            # Convert the image value range to [0, 1].
+            sr = (sr + 1) / 2
+            hr = (hr + 1) / 2
+
             # The MSE Loss of the generated fake high-resolution image and real high-resolution image is calculated.
-            total_psnr_value += 10 * torch.log10(1. / mse_loss(sr, hr))
+            total_psnr_value += psnr_loss(sr, hr)
             # The SSIM of the generated fake high-resolution image and real high-resolution image is calculated.
             total_ssim_value += ssim_loss(sr, hr)
-            # The GMSD of the generated fake high-resolution image and real high-resolution image is calculated.
-            total_gmsd_value += gmsd_loss(sr, hr)
 
             progress_bar.set_description(f"PSNR: {total_psnr_value / (i + 1):.2f} "
-                                         f"SSIM: {total_ssim_value / (i + 1):.4f} "
-                                         f"GMSD: {total_gmsd_value / (i + 1):.4f}")
+                                         f"SSIM: {total_ssim_value / (i + 1):.4f}")
 
-    return total_psnr_value / total, total_ssim_value / total, total_gmsd_value / total
+    out = total_psnr_value / total, total_ssim_value / total
+
+    return out
