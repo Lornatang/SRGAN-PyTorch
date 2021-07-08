04f5c903d30a73ae8159706ba40478b349540ce7
Liu Changyu <liuchangyu1111@gmail.com>
2021-07-08 10:55:48 +0800

    Update create_dataset.py

--- scripts/create_dataset.py
+++ scripts/create_dataset.py
@@ -25,13 +25,17 @@ def main():
     r""" Make training data set and validation data set."""
     total_image_lists = os.listdir(os.path.join("train", "input"))
     # The original data set is divided into 9:1 (train:test)
-    test_image_lists = random.sample(total_image_lists, int(len(total_image_lists) / 10))
+    test_image_lists = random.sample(total_image_lists,
+                                     int(len(total_image_lists) / 10))
     # Move the validation set to the specified location.
     for test_image_name in test_image_lists:
-        logger.info(f"Process: `{os.path.join('train', 'input', test_image_name)}`.")
+        filename = os.path.join("train", "input", test_image_name)
+        logger.info(f"Process: `{filename}`.")
         # Move the test image into the test folder.
-        shutil.move(os.path.join("train", "input", test_image_name), os.path.join("test", "input", test_image_name))
-        shutil.move(os.path.join("train", "target", test_image_name), os.path.join("test", "target", test_image_name))
+        shutil.move(os.path.join("train", "input", test_image_name),
+                    os.path.join("test", "input", test_image_name))
+        shutil.move(os.path.join("train", "target", test_image_name),
+                    os.path.join("test", "target", test_image_name))
 
 
 if __name__ == "__main__":
