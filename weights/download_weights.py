d85fb74fd15dbcd4f505824cbfe2fe0898ed685b
Liu Changyu <liuchangyu1111@gmail.com>
2021-07-09 16:13:39 +0800

    Update download_weights.py

--- weights/download_weights.py
+++ weights/download_weights.py
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-from srgan_pytorch.models.generator import srgan
+from srgan_pytorch.model import generator
 
-model = srgan(pretrained=True)
+model = generator(pretrained=True)
 print(model)
