9b4ad0ff2c5262caf65bafed3470cebeed77e556
Liu Changyu <liuchangyu1111@gmail.com>
2021-07-09 16:08:52 +0800

    Update common.py

--- srgan_pytorch/utils/common.py
+++ srgan_pytorch/utils/common.py
@@ -10,27 +10,37 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==============================================================================
+# ============================================================================
 import logging
 import os
 
-__all__ = ["create_folder", "check_image_file", "AverageMeter", "ProgressMeter"]
+__all__ = ["create_folder", "check_image_file"]
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(format="[ %(levelname)s ] %(message)s", level=logging.INFO)
 
 
-def create_folder(folder):
+def create_folder(dir_name):
+    r""" Create folder, this is a relative path.
+    
+    Args:
+        dir_name (str): The name of the folder to be created.
+    """
+    dir_path = os.path.join(os.getcwd(), dir_name)
     try:
-        os.makedirs(folder)
-        logger.info(f"Create `{os.path.join(os.getcwd(), folder)}` directory successful.")
+        os.makedirs(dir_name)
+        logger.info(f"Create `{dir_path}` directory successful.")
     except OSError:
-        logger.warning(f"Directory `{os.path.join(os.getcwd(), folder)}` already exists!")
+        logger.warning(f"Directory `{dir_path}` already exists!")
         pass
 
 
 def check_image_file(filename):
-    r"""Filter non image files in directory.
+    r""" Filter non image files in directory.
+
+    Note:
+        The performance of the model trained using `png, bmp` 
+        compression format images is generally better.
 
     Args:
         filename (str): File name under path.
@@ -38,48 +48,7 @@ def check_image_file(filename):
     Returns:
         Return True if bool(x) is True for any x in the iterable.
     """
-    return any(filename.endswith(extension.lower()) for extension in [".jpeg", ".png", ".bmp", "tiff"])
-
-
-# Copy from https://github.com/pytorch/examples/blob/master/imagenet/main.py
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-
-    def __init__(self, name, fmt=':f'):
-        self.name = name
-        self.fmt = fmt
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-    def __str__(self):
-        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
-        return fmtstr.format(**self.__dict__)
-
-
-# Copy from https://github.com/pytorch/examples/blob/master/imagenet/main.py
-class ProgressMeter(object):
-    def __init__(self, num_batches, meters, prefix=""):
-        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
-        self.meters = meters
-        self.prefix = prefix
-
-    def display(self, batch):
-        entries = [self.prefix + self.batch_fmtstr.format(batch)]
-        entries += [str(meter) for meter in self.meters]
-        print('\t'.join(entries))
-
-    def _get_batch_fmtstr(self, num_batches):
-        num_digits = len(str(num_batches // 1))
-        fmt = '{:' + str(num_digits) + 'd}'
-        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+    support_format = [
+        "jpg", "jpeg", "png", "bmp", "tiff", "JPG", "JPEG", "PNG", "BMP", "TIFF"
+    ]
+    return any(filename.endswith(extension) for extension in support_format)
