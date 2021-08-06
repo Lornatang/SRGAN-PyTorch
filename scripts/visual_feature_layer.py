0506da3e3dc77629a5af99f37b6ae43b71348920
Liu Changyu <liuchangyu1111@gmail.com>
2021-08-06 14:55:50 +0800

    Update visual_feature_layer.py

--- scripts/visual_feature_layer.py
+++ scripts/visual_feature_layer.py
@@ -11,23 +11,92 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-import matplotlib.pyplot as plt
-import torch.nn as nn
-import torchvision.models as models
+import os
+import torch
+import torchvision as tv
 import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
+import argparse
+import skimage.data
+import skimage.io
+import skimage.transform
+import numpy as np
+import matplotlib.pyplot as plt
 from PIL import Image
+import cv2
+from model import Generator
 
-image = transforms.ToTensor()(Image.open("visual/SR/104_27.png"))
+class FeatureExtractor(nn.Module):
+    def __init__(self, submodule, extracted_layers):
+        super(FeatureExtractor, self).__init__()
+        self.submodule = submodule
+        self.extracted_layers = extracted_layers
+ 
+    def forward(self, x):
+        outputs = {}
+        for name, module in self.submodule._modules.items():            
+            x = module(x)
+            print(name)
+            if self.extracted_layers is None or name in self.extracted_layers and 'fc' not in name:
+                outputs[name] = x
 
-vgg19 = models.vgg19(pretrained=True).eval()
-model = nn.Sequential(*list(vgg19.features.children())[:36])
+        return outputs
 
-for _, parameters in model.named_parameters():
-    parameters.requires_grad = False
 
-features = model(image).squeeze(0)
+def get_picture(pic_name, transform):
+    img = skimage.io.imread(pic_name)
+    img = skimage.transform.resize(img, (256, 256))
+    img = np.asarray(img, dtype=np.float32)
+    return transform(img)
 
-for i in range(1, 2):
-    plt.subplot(1, 1, i)
-    new_img_PIL = transforms.ToPILImage()(features[i - 1]).convert()
-    plt.savefig("features.png")
+def make_dirs(path):
+    if os.path.exists(path) is False:
+        os.makedirs(path)
+
+
+def get_feature():
+    pic_dir = 'lr_024067.bmp'
+    transform = transforms.ToTensor()
+    img = get_picture(pic_dir, transform)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    img = img.unsqueeze(0)
+
+    img = img.to(device)
+
+    
+    net = Generator().to(device)
+    net.load_state_dict(torch.load('20210728.pth'))
+    exact_list = None
+    dst = './feautures'
+    therd_size = 64
+
+    myexactor = FeatureExtractor(net, exact_list)
+    outs = myexactor(img)
+    for k, v in outs.items():
+        features = v[0]
+        iter_range = features.shape[0]
+        for i in range(iter_range):
+            #plt.imshow(x[0].data.numpy()[0,i,:,:],cmap='jet')
+
+            feature = features.data.cpu().numpy()
+            feature_img = feature[i,:,:]
+            feature_img = np.asarray(feature_img * 255, dtype=np.uint8)
+            
+            dst_path = os.path.join(dst, k)
+            
+            make_dirs(dst_path)
+            feature_img = cv2.applyColorMap(feature_img, cv2.COLORMAP_JET)
+            if feature_img.shape[0] < therd_size:
+                tmp_file = os.path.join(dst_path, str(i) + '_' + str(therd_size) + '.png')
+                tmp_img = feature_img.copy()
+                tmp_img = cv2.resize(tmp_img, (therd_size,therd_size), interpolation =  cv2.INTER_NEAREST)
+                cv2.imwrite(tmp_file, tmp_img)
+            
+            dst_file = os.path.join(dst_path, str(i) + '.png')
+            cv2.imwrite(dst_file, feature_img)
+
+if __name__ == '__main__':
+    get_feature()
+
+
