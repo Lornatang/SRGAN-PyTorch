af8ebfc67c067eec01a0dceae062657f59c1d6c0
Liu Changyu <liuchangyu1111@gmail.com>
2021-08-06 14:53:57 +0800

    Update download_dataset.sh

--- data/download_dataset.sh
+++ data/download_dataset.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # Training.
-echo "Downloading DIV2K training dataset."
+echo "Downloading DIV2K train dataset."
 wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip
 unzip DIV2K_train_HR.zip
 mv DIV2K_train_HR train
 rm DIV2K_train_HR.zip
 
+echo "Downloading DIV2K valid dataset."
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip
+unzip DIV2K_valid_HR.zip
+mv DIV2K_valid_HR val
+rm DIV2K_valid_HR.zip
+
 # Testing.
-echo "Download Set5/Set14 testing dataset see README.md#Test"
+echo "Download Set5/Set14 test dataset see README.md#Test"
