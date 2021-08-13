a5cdcb9952756e19bdb383cf541d392ed6b08236
Liu Changyu <liuchangyu1111@gmail.com>
2021-08-13 09:42:29 +0800

    Update train.py

--- train.py
+++ train.py
@@ -53,13 +53,14 @@ def main() -> None:
             loss.backward()
             p_optimizer.step()
             # Print Loss.
-            print(f"Epoch[{epoch:05d}/{p_epochs:05d}]({index:05d}/{num_batches:05d}) Loss: {loss.item():.4f}.")
-            batches = index + epoch * num_batches + 1
+            print(f"Epoch[{epoch:03d}/{p_epochs:03d}]({index:04d}/{num_batches:04d}) Loss: {loss.item():.6f}.")
+            batches = index + epoch * num_batches
             writer.add_scalar("PSNR/Loss", loss.item(), batches)
+            
         # Save checkpoint model.
         torch.save(generator.state_dict(), os.path.join("samples", f"P_epoch{epoch}.pth"))
     # Save final model.
-    torch.save(generator.state_dict(), os.path.join("results", "P-last.pth"))
+    torch.save(generator.state_dict(), os.path.join("results", p_filename))
 
     # Train GAN-Oral.
     for epoch in range(start_g_epoch, g_epochs):
@@ -71,45 +72,66 @@ def main() -> None:
             real_label = torch.full([label_size, 1], 1.0, dtype=lr.dtype, device=device)
             fake_label = torch.full([label_size, 1], 0.0, dtype=lr.dtype, device=device)
             ##############################################
-            # (1) Update D network: E(HR)[log(D(HR))] + E(SR)[log(1 - D(G(SR))]
+            # (1) Update D network.
             ##############################################
+            # Fix the generator and update the discriminator.
+            for parameters in discriminator.parameters():
+                parameters.requires_grad = True
+            for parameters in generator.parameters():
+                parameters.requires_grad = False
+            # Set the discriminator gradient to 0.
             discriminator.zero_grad()
+
             # Train real sample with discriminator.
             hr_output = discriminator(hr)
             d_loss_hr = adversarial_criterion(hr_output, real_label)
             d_loss_hr.backward()
-            d_hr = hr_output.mean().item()
 
-            # Train fake sample with discriminator.
+            # Generate a super-resolution image.
             sr = generator(lr)
+            
+            # Train fake sample with discriminator.
             sr_output = discriminator(sr.detach())
             d_loss_sr = adversarial_criterion(sr_output, fake_label)
             d_loss_sr.backward()
-            d_sr1 = sr_output.mean().item()
+
+            # Update the discriminator weights.
             d_loss = d_loss_hr + d_loss_sr
             d_optimizer.step()
+            d_hr = hr_output.mean().item()
+            d_sr1 = sr_output.mean().item()
             ##############################################
-            # (2) Update G network: E(SR)[log(1 - D(G(SR))]
+            # (2) Update G network.
             ##############################################
+            # Fix the discriminator and update the generator.
+            for parameters in discriminator.parameters():
+                parameters.requires_grad = False
+            for parameters in generator.parameters():
+                parameters.requires_grad = True
+            # Set the generator gradient to 0.
             generator.zero_grad()
-
+            
             # Train fake sample with generator.
-            sr = generator(lr)
             sr_output = discriminator(sr)
-            pixel_loss       = pixel_weight       * pixel_criterion(sr, hr.detach())
             perceptual_loss  = perceptual_weight  * perceptual_criterion(sr, hr.detach())
             adversarial_loss = adversarial_weight * adversarial_criterion(sr_output, real_label)
-            d_sr2 = sr_output.mean().item()
-            g_loss = pixel_loss + perceptual_loss + adversarial_loss
+
+            # Update the generator weights.
+            g_loss = perceptual_loss + adversarial_loss
             g_loss.backward()
             g_optimizer.step()
+            d_sr2 = sr_output.mean().item()
             # Print Loss.
-            print(f"Epoch[{epoch:05d}/{g_epochs:05d}]({index:05d}/{num_batches:05d}) "
-                  f"D Loss: {d_loss.item():.4f} G Loss: {g_loss.item():.4f}"
-                  f"D(HR): {d_hr} D(SR1)/D(SR2): {d_sr1}/{d_sr2}.")
-            batches = index + epoch * num_batches + 1
+            print(f"Epoch[{epoch:02d}/{g_epochs:02d}]({index:04d}/{num_batches:04d}) "
+                  f"D Loss: {d_loss.item():.6f} G Loss: {g_loss.item():.6f} "
+                  f"D(HR): {d_hr:.6f} D(SR1)/D(SR2): {d_sr1:.6f}/{d_sr2:.6f}.")
+            batches = index + epoch * num_batches
             writer.add_scalar("GAN/D_Loss", d_loss.item(), batches)
             writer.add_scalar("GAN/G_Loss", g_loss.item(), batches)
+            writer.add_scalar("GAN/D_HR",   d_hr,          batches)
+            writer.add_scalar("GAN/D_SR1",  d_sr1,         batches)
+            writer.add_scalar("GAN/D_SR2",  d_sr2,         batches)
+
         # Adjust learning rate.
         d_scheduler.step()
         g_scheduler.step()
@@ -117,8 +139,8 @@ def main() -> None:
         torch.save(discriminator.state_dict(), os.path.join("samples", f"D_epoch{epoch}.pth"))
         torch.save(generator.state_dict(),     os.path.join("samples", f"G_epoch{epoch}.pth"))
     # Save final model.
-    torch.save(discriminator.state_dict(), os.path.join("results", "D-last.pth"))
-    torch.save(generator.state_dict(),     os.path.join("results", "G-last.pth"))
+    torch.save(discriminator.state_dict(), os.path.join("results", d_filename))
+    torch.save(generator.state_dict(),     os.path.join("results", g_filename))
 
 
 if __name__ == "__main__":
