5c96df0a7d4846280dafe0a9b59efcdb64599c80
Liu Changyu <liuchangyu1111@gmail.com>
2021-08-13 09:42:48 +0800

    Update validate.py

--- validate.py
+++ validate.py
@@ -16,6 +16,8 @@
 # File description: Realize the verification function after model training.
 # ==============================================================================
 
+import shutil
+
 import skimage.color
 import skimage.io
 import skimage.metrics
@@ -53,27 +55,38 @@ def image_quality_assessment(sr_path: str, hr_path: str) -> Tuple[float, float]:
 
 
 def main() -> None:
-    net.half()
-    net.eval()
+    # Create exp dir.
+    if os.path.exists(sr_dir):
+        shutil.rmtree(sr_dir)
+    os.makedirs(sr_dir)
 
+    net.load_state_dict(torch.load(model_path, map_location=device))
+    # Set model to eval mode.
+    net.eval()
+    # Enable half inference.
+    net.half()
+
+    # Init IQA.
     total_psnr = 0.0
     total_ssim = 0.0
 
+    # Get test filename.
     filenames = os.listdir(lr_dir)
+    # How many number of test filenames.
     total_files = len(filenames)
 
     for index in range(total_files):
         lr_path = os.path.join(lr_dir, filenames[index])
         sr_path = os.path.join(sr_dir, filenames[index])
         hr_path = os.path.join(hr_dir, filenames[index])
-        # LR to SR.
+        # LR->SR.
         lr_tensor = image2tensor(Image.open(lr_path)).unsqueeze(0).to(device)
         with torch.no_grad():
             sr_tensor = net(lr_tensor.half())
             torchvision.utils.save_image(sr_tensor, sr_path)
             
-        # Test PSNR and SSIM.
-        print(f"Test `{lr_path}`.")
+        # IQA.
+        print(f"Test `{os.path.abspath(lr_path)}`.")
         psnr, ssim = image_quality_assessment(sr_path, hr_path)
         total_psnr += psnr
         total_ssim += ssim
