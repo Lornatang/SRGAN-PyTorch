a2306e3986ae1c18d7c004cec71245aeee1ea55f
Liu Changyu <liuchangyu1111@gmail.com>
2021-09-02 10:08:16 +0800

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,6 @@
 # ==============================================================================
 # File description: Realize the parameter configuration function of data set, model, training and verification code.
 # ==============================================================================
-
 import os
 
 import torch
@@ -32,75 +31,73 @@ from model import PerceptualLoss
 # ==============================================================================
 #                              Common configure
 # ==============================================================================
-torch.manual_seed(0)
-cudnn.benchmark = True
-cudnn.deterministic = False
-device = torch.device("cuda:0")
-# Runing mode.
-mode = "train"
-scale_factor = 4
-# Exp name.
-exp_name = "exp001"
+torch.manual_seed(0)                       # Set random seed.
+upscale_factor   = 4                       # How many times the size of the high-resolution image in the data set is than the low-resolution image.
+device           = torch.device("cuda:0")  # Use the first GPU for processing by default.
+cudnn.benchmark  = True                    # If the dimension or type of the input data of the network does not change much, turn it on, otherwise turn it off.
+mode             = "train"                 # Run mode. Specific mode loads specific variables.
+exp_name         = "exp000"                # Experiment name.
 
 # ==============================================================================
 #                              Train configure
 # ==============================================================================
 if mode == "train":
-    # 1. Dataset path.
-    dataroot              = "data/ImageNet"
-    image_size            = 96
-    batch_size            = 16
+    # 1. Configure dataset.
+    train_dir             = "data/ImageNet/train"                 # The address of the training data set.
+    valid_dir             = "data/ImageNet/valid"                 # Verify the address of the data set.
+    image_size            = 96                                    # High-resolution image size in the training data set.
+    batch_size            = 16                                    # Training data batch size.
 
-    # 2. Define model.
-    discriminator         = Discriminator(image_size).to(device)
-    generator             = Generator().to(device)
+    # 2. Configure model.
+    discriminator         = Discriminator().to(device)            # Load the discriminator model.
+    generator             = Generator().to(device)                # Load the generative model.
 
-    # 3. Reume training.
-    start_p_epoch         = 0                                              
-    start_g_epoch         = 0                                              
-    resume                = False                                          
-    resume_p_weight       = ""                                             
-    resume_d_weight       = ""                                             
-    resume_g_weight       = ""                                             
+    # 3. Resume training.
+    start_p_epoch         = 0                                     # The number of initial iterations of the generator training phase. When set to 0, it means incremental training.
+    start_epoch           = 0                                     # The number of initial iterations of the adversarial network training. When set to 0, it means incremental training.
+    resume                = False                                 # Set to `True` to continue training from the previous training progress.
+    resume_p_weight       = ""                                    # Restore the weight of the generative model during generator training.
+    resume_d_weight       = ""                                    # Restore the weight of the generative model during the training of the adversarial network.
+    resume_g_weight       = ""                                    # Restore the weight of the discriminator model during the training of the adversarial network.
 
-    # 4. Number of epochs.
-    p_epochs              = 45                                         
-    g_epochs              = 9
-                                             
+    # 4. Train epochs.
+    p_epochs              = 128                                   # The total number of cycles of the generator training phase.
+    epochs                = 32                                    # The total number of cycles in the training phase of the adversarial network.
+
     # 5. Loss function.
-    pixel_criterion       = nn.MSELoss().to(device)                        
-    perceptual_criterion  = PerceptualLoss().to(device)                   
-    adversarial_criterion = nn.BCELoss().to(device)           
+    pixel_criterion       = nn.MSELoss().to(device)               # Pixel loss.
+    perceptual_criterion  = PerceptualLoss().to(device)           # Perceived loss.
+    adversarial_criterion = nn.BCELoss().to(device)               # Fight against loss.
     # Loss function weight.
-    perceptual_weight     = 6e-03
-    adversarial_weight    = 1e-03
+    pixel_weight          = 1.0
+    perceptual_weight     = 0.06
+    adversarial_weight    = 0.001
 
     # 6. Optimizer.
-    p_optimizer           = optim.Adam(generator.parameters(),     1e-4) 
-    d_optimizer           = optim.Adam(discriminator.parameters(), 1e-4) 
-    g_optimizer           = optim.Adam(generator.parameters(),     1e-4) 
+    p_optimizer           = optim.Adam(generator.parameters(),     0.0001, (0.9, 0.999))  # Generate model learning rate during generator training.
+    d_optimizer           = optim.Adam(discriminator.parameters(), 0.0001, (0.9, 0.999))  # Discriminator learning rate during adversarial network training.
+    g_optimizer           = optim.Adam(generator.parameters(),     0.0001, (0.9, 0.999))  # The learning rate of the generator during network training.
 
-    # 7. Leaning scheduler.
-    d_scheduler           = StepLR(d_optimizer, g_epochs // 2)           
-    g_scheduler           = StepLR(g_optimizer, g_epochs // 2)           
+    # 7. Scheduler.
+    d_scheduler           = StepLR(d_optimizer, epochs // 2)  # Identify the model scheduler during adversarial training.
+    g_scheduler           = StepLR(g_optimizer, epochs // 2)  # Generate model scheduler during adversarial training.
 
     # 8. Training log.
-    writer                = SummaryWriter(os.path.join("samples", "logs", exp_name))
+    writer                = SummaryWriter(os.path.join("samples",  "logs", exp_name))
 
-
-    # Exp model name.
-    p_filename            = f"P-{exp_name}.pth"
-    d_filename            = f"D-{exp_name}.pth"
-    g_filename            = f"G-{exp_name}.pth"
+    # Additional variables.
+    exp_dir1 = os.path.join("samples", exp_name)
+    exp_dir2 = os.path.join("results", exp_name)
 
 # ==============================================================================
 #                              Validate configure
 # ==============================================================================
 if mode == "validate":
-    net        = Generator().to(device)
-    # Model weight path.
-    model_path = f"results/G-{exp_name}.pth"
-    # Test dataset path.
-    lr_dir     = f"data/Set5/LRbicx4"
-    sr_dir     = f"results/{exp_name}"
-    hr_dir     = f"data/Set5/GTmod12"
+    exp_dir    = os.path.join("results", "test", exp_name)  # Additional variables.
+
+    model      = Generator().to(device)                     # Load the super-resolution model.
+    model_path = f"results/{exp_name}/g-best.pth"           # Model weight address.
+    lr_dir     = f"data/Set5/LRbicx4"                       # Low resolution image address.
+    sr_dir     = f"results/test/{exp_name}"                 # Super-resolution image address.
+    hr_dir     = f"data/Set5/GTmod12"                       # High resolution image address.
+
