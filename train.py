3be30c29689d1e1b1859963da767d3636b58882d
Liu Changyu <liuchangyu1111@gmail.com>
2021-09-09 20:16:47 +0800

    Update train.py

--- train.py
+++ train.py
@@ -22,93 +22,102 @@ from dataset import BaseDataset
 
 
 def train_generator(train_dataloader, epoch) -> None:
-    # The purpose of training the generator is to minimize the pixel difference
-    # between super-resolution images and high-resolution images.
-    batches = len(train_dataloader)
+    """Only train the generative model.
 
-    # Start training mode.
+    Args:
+        train_dataloader (torch.utils.data.DataLoader): The loader of the training data set.
+        epoch (int): number of training cycles.
+
+    """
+    # Calculate how many iterations there are under Epoch.
+    batches = len(train_dataloader)
+    # Put the generative model in training mode.
     generator.train()
 
     for index, (lr, hr) in enumerate(train_dataloader):
-        # 0. Copy the data to the designated device.
+        # Copy the data to the specified device.
         lr = lr.to(device)
         hr = hr.to(device)
-        # 1. Set the gradient of the generated model to 0.
+        # Initialize the gradient of the generated model.
         generator.zero_grad()
-        # 2. Calculate the difference between the super-resolution image and the high-resolution image at the pixel level.
+        # Generate super-resolution images.
         sr = generator(lr)
+        # Calculate the difference between the super-resolution image and the high-resolution image at the pixel level.
         pixel_loss = pixel_criterion(sr, hr)
-        # 3. Update the weights of the generative model.
+        # Update the weights of the generated model.
         pixel_loss.backward()
         p_optimizer.step()
-
-        # Write the loss value in the process of training and generating the network into Tensorboard.
+        # Write the loss during training into Tensorboard.
         iters = index + epoch * batches + 1
-        writer.add_scalar("Train_Generator/Loss", pixel_loss.item(), iters)
-        # Output the loss function every ten iterations.
+        writer.add_scalar("Train/Loss", pixel_loss.item(), iters)
+        # Print the loss function every ten iterations and the last iteration in this Epoch.
         if (index + 1) % 10 == 0 or (index + 1) == batches:
-            print(f"Train stage: generator "
-                  f"Epoch[{epoch + 1:04d}/{p_epochs:04d}]({index + 1:05d}/{batches:05d}) "
+            print(f"Train Epoch[{epoch + 1:04d}/{p_epochs:04d}]({index + 1:05d}/{batches:05d}) "
                   f"Loss: {pixel_loss.item():.6f}.")
 
 
 def train_adversarial(train_dataloader, epoch) -> None:
-    # The purpose of training the generator is to minimize the pixel difference between super-resolution images and high-resolution images.
-    batches = len(train_dataloader)
+    """Training generative models and adversarial models.
 
-    # Start training mode.
+    Args:
+        train_dataloader (torch.utils.data.DataLoader): The loader of the training data set.
+        epoch (int): number of training cycles.
+
+    """
+    # Calculate how many iterations there are under Epoch.
+    batches = len(train_dataloader)
+    # Put the two models in training mode.
     discriminator.train()
     generator.train()
 
     for index, (lr, hr) in enumerate(train_dataloader):
-        # 0. Copy the data to the designated device.
+        # Copy the data to the specified device.
         lr = lr.to(device)
         hr = hr.to(device)
-
         label_size = lr.size(0)
-        # 1. Tagging. Set the real sample label to 1, and the false sample label to 0.
+        # 打label. Set the real sample label to 1, and the false sample label to 0.
         real_label = torch.full([label_size, 1], 1.0, dtype=lr.dtype, device=device)
         fake_label = torch.full([label_size, 1], 0.0, dtype=lr.dtype, device=device)
 
-        # 2. Set the discriminator gradient to 0.
+        # Initialize the identification model gradient.
         discriminator.zero_grad()
-        # 3. Calculate the loss of the discriminator on the high-resolution image.
+        # Calculate the loss of the identification model on the high-resolution image.
         output = discriminator(hr)
         d_loss_hr = adversarial_criterion(output, real_label)
         d_loss_hr.backward()
         d_hr = output.mean().item()
-        # 4. Calculate the loss of the discriminator on the super-resolution image.
+        # Generate super-resolution images.
         sr = generator(lr)
+        # Calculate the loss of the identification model on the super-resolution image.
         output = discriminator(sr.detach())
         d_loss_sr = adversarial_criterion(output, fake_label)
         d_loss_sr.backward()
         d_sr1 = output.mean().item()
-        # 5. Update the discriminator weights.
+        # Update the weights of the authentication model.
         d_loss = d_loss_hr + d_loss_sr
         d_optimizer.step()
 
-        # 6. Set the generator gradient to 0.
+        # Initialize the gradient of the generated model.
         generator.zero_grad()
-        # 7. Calculate the loss of the generator on the super-resolution image.
+        # Calculate the loss of the identification model on the super-resolution image.
         output = discriminator(sr)
-        # Weighted loss. 0.006 * perceptual loss + 0.001 * adversarial loss.
-        pixel_loss = pixel_weight * pixel_criterion(sr, hr.detach())
-        perceptual_loss = perceptual_weight * perceptual_criterion(sr, hr.detach())
+        # Perceptual loss=0.006 * content loss + 0.001 * adversarial loss.
+        perceptual_loss = content_weight * content_criterion(sr, hr.detach())
         adversarial_loss = adversarial_weight * adversarial_criterion(output, real_label)
-        # 8. Update generator weights.
-        g_loss = pixel_loss + perceptual_loss + adversarial_loss
+        # Update the weights of the generated model.
+        g_loss = perceptual_loss + adversarial_loss
         g_loss.backward()
         g_optimizer.step()
         d_sr2 = output.mean().item()
 
-        # Write the loss value in the process of training the adversarial network into Tensorboard.
+        # Write the loss during training into Tensorboard.
         iters = index + epoch * batches + 1
         writer.add_scalar("Train_Adversarial/D_Loss", d_loss.item(), iters)
         writer.add_scalar("Train_Adversarial/G_Loss", g_loss.item(), iters)
         writer.add_scalar("Train_Adversarial/D_HR", d_hr, iters)
         writer.add_scalar("Train_Adversarial/D_SR1", d_sr1, iters)
         writer.add_scalar("Train_Adversarial/D_SR2", d_sr2, iters)
-        # Output the loss function every ten iterations.
+        # Print the loss function every ten iterations and the last iteration in this Epoch.
         if (index + 1) % 10 == 0 or (index + 1) == batches:
             print(f"Train stage: adversarial "
                   f"Epoch[{epoch + 1:04d}/{epochs:04d}]({index + 1:05d}/{batches:05d}) "
@@ -117,19 +126,30 @@ def train_adversarial(train_dataloader, epoch) -> None:
 
 
 def validate(valid_dataloader, epoch, stage) -> float:
-    # Verify the performance of the model after each Epoch.
-    batches = len(valid_dataloader)
-    total_psnr_value = 0.0
+    """Verify the generative model.
 
-    # Start verification mode.
+    Args:
+        valid_dataloader (torch.utils.data.DataLoader): loader for validating data set.
+        epoch (int): number of training cycles.
+        stage (str): In which stage to verify, one is `generator`, the other is `adversarial`.
+
+    Returns:
+        PSNR value(float).
+
+    """
+    # Calculate how many iterations there are under Epoch.
+    batches = len(valid_dataloader)
+    # Put the generated model in verification mode.
     generator.eval()
+    # Initialize the evaluation index.
+    total_psnr_value = 0.0
 
     with torch.no_grad():
         for index, (lr, hr) in enumerate(valid_dataloader):
             # Copy the data to the specified device.
             lr = lr.to(device)
             hr = hr.to(device)
-            # Calculate the loss.
+            # Generate super-resolution images.
             sr = generator(lr)
             # Calculate the PSNR indicator.
             mse_loss = ((sr - hr) ** 2).data.mean()
@@ -149,7 +169,7 @@ def validate(valid_dataloader, epoch, stage) -> float:
 
 
 def main() -> None:
-    # Create a folder of super-resolution experiment results.
+    # Create a super-resolution experiment result folder.
     if not os.path.exists(exp_dir1):
         os.makedirs(exp_dir1)
     if not os.path.exists(exp_dir2):
@@ -160,8 +180,8 @@ def main() -> None:
     valid_dataset = BaseDataset(valid_dir, image_size, upscale_factor, "valid")
     train_dataloader = DataLoader(train_dataset, batch_size, True, pin_memory=True)
     valid_dataloader = DataLoader(valid_dataset, batch_size, False, pin_memory=True)
-    # Check whether the training progress of the last abnormal end is restored, for example,
-    # the power is cut off in the middle of the training.
+    # Check whether the training progress of the last abnormal end is restored, for example, the power is
+    # cut off in the middle of the training.
     if resume:
         print("Resuming...")
         if resume_p_weight != "":
@@ -170,50 +190,51 @@ def main() -> None:
             discriminator.load_state_dict(torch.load(resume_d_weight))
             generator.load_state_dict(torch.load(resume_g_weight))
 
-    # Initialize the evaluation index of the generator training phase
+    # Initialize the evaluation indicators for the training stage of the generative model
     best_psnr_value = 0.0
-    # Train the generator stage.
+    # Train the generative model stage.
     for epoch in range(start_p_epoch, p_epochs):
-        # Train the generator model under each Epoch.
+        # Train each Epoch to generate a model.
         train_generator(train_dataloader, epoch)
-        # Verify the model performance after each Epoch.
+        # Verify each generation model under Epoch.
         psnr_value = validate(valid_dataloader, epoch, "generator")
-        # Evaluate whether the performance of the current node model is the highest indicator.
+        # Determine whether the performance of the generated model under Epoch is the best.
         is_best = psnr_value > best_psnr_value
         best_psnr_value = max(psnr_value, best_psnr_value)
-        # Save the node model. If the current node model has the best performance,
-        # a model file ending with `-best` will also be saved.
+        # Save the weight of the generated model under Epoch. If the performance of the generated model under Epoch
+        # is best, save a file ending with `-best.pth` in the `results` directory.
         torch.save(generator.state_dict(), os.path.join(exp_dir1, f"p_epoch{epoch + 1}.pth"))
         if is_best:
             torch.save(generator.state_dict(), os.path.join(exp_dir2, "p-best.pth"))
 
-    # Save the training model for the last iteration of the training generator phase.
+    # Save the weight of the last generated model under Epoch in this stage.
     torch.save(generator.state_dict(), os.path.join(exp_dir2, "p-last.pth"))
 
-    # Initialize the evaluation index of the adversarial network training stage.
+    # Initialize the evaluation index of the adversarial model training phase.
     best_psnr_value = 0.0
-    # Load the model with the best performance in the training phase of the generator.
+    # Load the model weights with the best indicators in the previous round of training.
     generator.load_state_dict(torch.load(os.path.join(exp_dir2, "p-best.pth")))
-    # Training against the network stage.
+    # Training the adversarial model stage.
     for epoch in range(start_epoch, epochs):
-        # Train the generator model and discriminator model under each Epoch.
+        # Train the adversarial model every time under Epoch.
         train_adversarial(train_dataloader, epoch)
-        # Verify the model performance after each Epoch.
+        # Verify the adversarial model every time under Epoch.
         psnr_value = validate(valid_dataloader, epoch, "adversarial")
-        # Evaluate whether the performance of the current node model is the highest indicator.
+        # Determine whether the performance of the adversarial model under Epoch is the best.
         is_best = psnr_value > best_psnr_value
         best_psnr_value = max(psnr_value, best_psnr_value)
-        # Save the node model. If the current node model has the best performance, a model file ending with `-best` will also be saved.
+        # Save the weight of the adversarial model under Epoch. If the performance of the adversarial model
+        # under Epoch is the best, it will save two additional files ending with `-best.pth` in the `results` directory.
         torch.save(discriminator.state_dict(), os.path.join(exp_dir1, f"d_epoch{epoch + 1}.pth"))
         torch.save(generator.state_dict(), os.path.join(exp_dir1, f"g_epoch{epoch + 1}.pth"))
         if is_best:
             torch.save(discriminator.state_dict(), os.path.join(exp_dir2, "d-best.pth"))
             torch.save(generator.state_dict(), os.path.join(exp_dir2, "g-best.pth"))
 
-    # Save the training model for the last iteration of the training adversarial network phase.
+    # Save the weight of the adversarial model under the last Epoch in this stage.
     torch.save(discriminator.state_dict(), os.path.join(exp_dir2, "d-last.pth"))
     torch.save(generator.state_dict(), os.path.join(exp_dir2, "g-last.pth"))
-    # Adjust the learning rate.
+    # Adjust the learning rate of the adversarial model.
     d_scheduler.step()
     g_scheduler.step()
 
