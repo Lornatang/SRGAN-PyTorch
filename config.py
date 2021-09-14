de3002f18234d2293c9fe28d1f36c0aa0bdecdd0
Liu Changyu <liuchangyu1111@gmail.com>
2021-09-14 18:14:21 +0800

    Update config.py

--- config.py
+++ config.py
@@ -43,45 +43,45 @@ exp_name         = "exp000"                # Experiment name.
 # ==============================================================================
 if mode == "train":
     # Configure dataset.
-    train_dir             = "data/ImageNet/train"       # The address of the training data set.
-    valid_dir             = "data/ImageNet/valid"       # Verify the address of the data set.
-    image_size            = 96                          # High-resolution image size in the training data set.
-    batch_size            = 16                          # Training data batch size.
+    train_dir             = "data/ImageNet/train"       # The address of the training dataset.
+    valid_dir             = "data/ImageNet/valid"       # The address of the validating dataset.
+    image_size            = 96                          # High-resolution image size in the training dataset.
+    batch_size            = 16                          # Dataset batch size.
 
     # Configure model.
     discriminator         = Discriminator().to(device)  # Load the discriminator model.
-    generator             = Generator().to(device)      # Load the generative model.
+    generator             = Generator().to(device)      # Load the generator model.
 
     # Resume training.
     start_p_epoch         = 0                           # The number of initial iterations of the generator training phase. When set to 0, it means incremental training.
-    start_epoch           = 0                           # The number of initial iterations of the adversarial network training. When set to 0, it means incremental training.
+    start_epoch           = 0                           # The number of initial iterations of the adversarial training phase. When set to 0, it means incremental training.
     resume                = False                       # Set to `True` to continue training from the previous training progress.
-    resume_p_weight       = ""                          # Restore the weight of the generative model during generator training.
-    resume_d_weight       = ""                          # Restore the weight of the generative model during the training of the adversarial network.
+    resume_p_weight       = ""                          # Restore the weight of the generator model during generator training.
+    resume_d_weight       = ""                          # Restore the weight of the generator model during the training of the adversarial network.
     resume_g_weight       = ""                          # Restore the weight of the discriminator model during the training of the adversarial network.
 
     # Train epochs.
-    p_epochs              = 46                          # The total number of cycles of the generator training phase.
-    epochs                = 10                          # The total number of cycles in the training phase of the adversarial network.
+    p_epochs              = 46                          # The total number of epochs of the generator training phase.
+    epochs                = 10                          # The total number of epochs of the adversarial training phase.
 
     # Loss function.
     psnr_criterion        = nn.MSELoss().to(device)     # PSNR metrics.
     pixel_criterion       = nn.MSELoss().to(device)     # Pixel loss.
     content_criterion     = ContentLoss().to(device)    # Content loss.
-    adversarial_criterion = nn.BCELoss().to(device)     # Fight against loss.
+    adversarial_criterion = nn.BCELoss().to(device)     # Adversarial loss.
     # Perceptual loss function weight.
     pixel_weight          = 0.01
     content_weight        = 1.0
     adversarial_weight    = 0.001
 
     # Optimizer.
-    p_optimizer           = optim.Adam(generator.parameters(),     0.0001, (0.9, 0.999))  # Generate model learning rate during generator training.
+    p_optimizer           = optim.Adam(generator.parameters(),     0.0001, (0.9, 0.999))  # Generator model learning rate during generator network training.
     d_optimizer           = optim.Adam(discriminator.parameters(), 0.0001, (0.9, 0.999))  # Discriminator learning rate during adversarial network training.
-    g_optimizer           = optim.Adam(generator.parameters(),     0.0001, (0.9, 0.999))  # The learning rate of the generator during network training.
+    g_optimizer           = optim.Adam(generator.parameters(),     0.0001, (0.9, 0.999))  # Generator learning rate during adversarial network training.
 
     # Scheduler.
-    d_scheduler           = StepLR(d_optimizer, epochs // 2, 0.1)  # Identify the model scheduler during adversarial training.
-    g_scheduler           = StepLR(g_optimizer, epochs // 2, 0.1)  # Generate model scheduler during adversarial training.
+    d_scheduler           = StepLR(d_optimizer, epochs // 2, 0.1)  # Discriminator model scheduler during adversarial network training.
+    g_scheduler           = StepLR(g_optimizer, epochs // 2, 0.1)  # Generator model scheduler during adversarial network training.
 
     # Training log.
     writer                = SummaryWriter(os.path.join("samples",  "logs", exp_name))
@@ -94,11 +94,15 @@ if mode == "train":
 #                              Validate configure
 # ==============================================================================
 if mode == "valid":
-    exp_dir    = os.path.join("results", "test", exp_name)  # Additional variables.
+    # Additional variables.
+    exp_dir    = os.path.join("results", "test", exp_name)
 
-    model      = Generator().to(device)                     # Load the super-resolution model.
-    model_path = f"results/{exp_name}/g-best.pth"           # Model weight address.
-    lr_dir     = f"data/Set5/LRbicx4"                       # Low resolution image address.
-    sr_dir     = f"results/test/{exp_name}"                 # Super-resolution image address.
-    hr_dir     = f"data/Set5/GTmod12"                       # High resolution image address.
+    # Load model.
+    model      = Generator().to(device)
+    model_path = f"results/{exp_name}/g-best.pth"
+
+    # Test data address.
+    lr_dir     = f"data/Set5/LRbicx4"
+    sr_dir     = f"results/test/{exp_name}"
+    hr_dir     = f"data/Set5/GTmod12"
 
