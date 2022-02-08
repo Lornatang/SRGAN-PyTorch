12aaa6a598abf2b19a18bd9af708ea48d202c35b
Lorna <liuchangyu1111@gmail.com>
2022-02-08 10:51:59 +0800

    Update run.py

--- scripts/run.py
+++ scripts/run.py
@@ -1,7 +1,7 @@
 import os
 
 # Prepare dataset
-os.system("python ./prepare_dataset.py --images_dir ../data/ImageNet/original --output_dir ../data/ImageNet/SRGAN/train --image_size 96 --step 48")
+os.system("python ./prepare_dataset.py --images_dir ../data/ImageNet/original --output_dir ../data/ImageNet/SRGAN/train --image_size 96 --step 48 --num_workers 10")
 
 # Split train and valid
 os.system("python ./split_train_valid_dataset.py --train_images_dir ../data/ImageNet/SRGAN/train --valid_images_dir ../data/ImageNet/SRGAN/valid --valid_samples_ratio 0.1")
