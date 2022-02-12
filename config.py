894a0e416f2c23311c97030d1b301bf3360214c3
Lorna <liuchangyu1111@gmail.com>
2022-02-12 23:09:05 +0800

    Update config.py

--- config.py
+++ config.py
@@ -110,4 +110,4 @@ if mode == "valid":
     sr_dir = f"results/test/{exp_name}"
     hr_dir = f"data/Set5/GTmod12"
 
-    model_path = f"results/{exp_name}/g-last.pth"、
+    model_path = f"results/{exp_name}/g-last.pth"
