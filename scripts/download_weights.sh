995382ca108e032711cac8c824f00b9475bcff4c
Lorna <liuchangyu1111@gmail.com>
2023-09-16 07:59:52 +0800

    Update download_weights.sh

--- scripts/download_weights.sh
+++ scripts/download_weights.sh
@@ -3,7 +3,6 @@ FILE=$1
 if [ "$FILE" == "SRGAN_x4-ImageNet" ]; then
   URL="https://huggingface.co/goodfellowliu/SRGAN-PyTorch/resolve/main/SRGAN_x4-ImageNet.pth.tar"
   FILE_PATH=./results/pretrained_models
-  mkdir -p ./data/ImageNet
   wget $URL -P $FILE_PATH
 elif [ "$FILE" == "SRResNet_x4-ImageNet" ]; then
   URL="https://huggingface.co/goodfellowliu/SRGAN-PyTorch/resolve/main/SRResNet_x4-ImageNet.pth.tar"
